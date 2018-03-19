68b168e9d1d38fcc45ca82b55671d694217bf1a0
chaoweb <1648907448@qq.com>
2018-03-19 22:32:30 +0800

    修改page报错

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,66 @@
-var appInstance  = getApp()
-console.log(appInstance.globalData) 
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text: "qimingdashazi",
+    array: [{mes: '1'}, {mes: '2'}]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+}) 
