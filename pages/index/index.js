5101d3edff448d7ee3c2bbe38abfaa7047b1e48f
chaoweb <1648907448@qq.com>
2018-03-25 18:29:11 +0800

    主页

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,21 +5,47 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    indicatorDots: false,
+    indicatorDots: true,
     autoplay: true,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    recommends: [{
+      message: '123',
+    }, {
+      message: '456',
+    }],
+    array: [{
+      message: [{
+        app: '热点互评',
+        url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '课堂助手',
+          url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '好文推荐',
+          url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '辩论大厅',
+          url: '../images/tools_lecihuodong.png',
+      }]
+    }, {
+      message: [{
+        app: '公开课堂',
+        url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '始创发表',
+          url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '帮扶平台',
+          url: '../images/tools_lecihuodong.png',
+      }, {
+          app: '敬请期待',
+          url: '../images/tools_lecihuodong.png',
+      }]
+    }
+    ]
   },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
+
   intervalChange: function (e) {
     this.setData({
       interval: e.detail.value
@@ -28,6 +54,15 @@ Page({
   durationChange: function (e) {
     this.setData({
       duration: e.detail.value
+    })
+  },
+  tagChoose: function (options) {
+    var that = this
+    var id = options.currentTarget.dataset.id;
+    console.log(id)
+    //设置当前样式
+    that.setData({
+      'currentItem': id
     })
   }
 })
